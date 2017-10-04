8ba316ded37653a5c9a3c1bbb6d5ded66cb60b1a
Axel Chauvin <axel.chauvin@emory.edu>
2017-10-04 10:06:17 -0400

    slight server changes

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const flash=require('connect-flash');
-mongoose.connect(process.env.MONGODB_URI);
 const nev=require('email-verification')(mongoose);
+
+const winston=require('winston')
+
+mongoose.connect(process.env.MONGODB_URI);
 app.set('views',__dirname+'/frontend');
 app.set('view engine','ejs')
-app.use(require('morgan')('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(require('cookie-parser')());
@@ -28,7 +29,6 @@ app.use(passport.session());
 app.use(flash())
 
 require('./config/nev.js')(nev);
-require('./backend/models/Users');
 require('./config/passport.js')(passport);
 require('./backend/appRoutes')(app,passport,nev);
 
