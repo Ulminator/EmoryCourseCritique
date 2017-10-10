567da9e0baf57509092e48d62d0d5549a739c4c8
Colin Jiang <colin08812@gmail.com>
2017-10-10 01:08:47 -0400

    layout fixes

--- frontend/containers/MainPageContainer.js
+++ frontend/containers/MainPageContainer.js
@@ -5,9 +5,10 @@ import Footer from "./components/Footer"
 
 class MainPageContainer extends Component {
   render() {
+    
     return(
 
-      <div>
+      <div style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}>
         <Body />
         <Footer />
       </div>
