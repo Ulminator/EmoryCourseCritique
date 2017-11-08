136b8194281455925a64df27d6591e1bc855f0e0
Matthew Ulmer <mulmer16@gmail.com>
2017-11-08 16:41:49 -0500

    changing import locations

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { configureStore, history } from './store/configureStore';
+// import { configureStore, history } from './store/configureStore';
+import { configureStore, history } from './store.js';
 import Root from './containers/Root';
-
+//
 import './assets/stylesheets/base.scss';
 import './assets/stylesheets/register.scss';
-
+//
+// // const store = configureStore({loginStatus: false});  //Added loginStatus
 const store = configureStore();
 
 render(
