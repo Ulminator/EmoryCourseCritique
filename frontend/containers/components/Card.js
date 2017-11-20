e63c3b2c80ddc820ba4529b784d30a69057d4094
chunkymankey <justinluo1998@gmail.com>
2017-11-20 01:36:41 -0500

    card color coded with rating

--- frontend/containers/components/Card.js
+++ frontend/containers/components/Card.js
@@ -31,35 +31,61 @@ class Card extends React.Component {
   
 
   render() {
-    
+    var rating = 0;
+    var ratingColor = "grey-text text-darken-2";
+    if(this.props.rating === "null"){
+        rating = "N/A";
+    }else if(this.props.rating > 4){ //its pretty good rating
+      rating = this.props.rating;
+      ratingColor = "green-text";
 
+    }else if(this.props.rating > 3){ //meh rating
+      rating = this.props.rating;
+      ratingColor = "lime-text";
+    }else if(this.props.rating > 2){ //garbo rating
+      rating = this.props.rating;
+      ratingColor = "orange-text";
+    }
+    else{ //disgusting
+      rating = this.props.rating;
+      ratingColor = "red-text text-lighten-1";
+    }
     return (
       <div className="card-panel white black-text" onClick={this.onClick} onMouseOver="" style={{cursor: 'pointer'}}>
-        <h5>
-          {this.props.cnum}:<span
+        <div className="row">
+          <div className="col s6">
+            <h5>
+              {this.props.professor}<span
+                style={{
+                  color: "#d18e01"
+                }}
+              >
+                {'\u00A0'}{}
+              </span>
+            </h5>
+            <span
+              style={{
+                fontWeight: 400
+              }}
+            >
+              {this.props.cnum}: {this.props.cname}
+            </span>{" "}
+          </div>
+          <div className ="col s4">
+          <h5 className={ratingColor}
             style={{
-              color: "#d18e01"
+              fontWeight: 300,
+              float: "right",
+              fontSize: '2.5rem',
+
             }}
           >
-            {this.props.cname}
-          </span>
-        </h5>
-        <span
-          style={{
-            fontWeight: 400
-          }}
-        >
-          {this.props.professor}
-        </span>{" "}
-        <br />
-        <span
-          style={{
-            fontWeight: 300
-          }}
-        >
-        Rating: {this.props.rating}
-        </span>
+            {rating}
+          </h5>
+        </div>
       </div>
+    </div>
+
     );
   }
 }
