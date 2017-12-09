d956fcaa6b7433eff1162486b4e455222d46b449
Colin Jiang <colin08812@gmail.com>
2017-12-09 00:56:25 -0500

    search backend redone, frontend in progress

--- backend/service/search.js
+++ backend/service/search.js
@@ -47,17 +47,17 @@ module.exports = function(req, res,next) {
     var this_resp = [];
     Course.find(query.query, query.select, query.cursor).then(function(courses) {
 
-        var count=0;
-        for (var i = 0; i < courses.length; i++) {
-        	count+=courses[i].professors.length;
-    	}
-        console.log(req);
-        console.log(query.select);
-        console.log(query.cursor);
-        console.log(courses);
-    	console.log(count);
+        var count=courses.length;
+        
+        
     	// Iterate thrugh courses
         courses.forEach(function(courseItem) {
+            var numProf = courseItem.professors.length;
+            var numRating = numProf;
+            var total_overall=0;
+            var total_difficulty=0;
+            var total_workload=0;
+            var this_sections=[];
         	// Iterate through each professor in each course
             courseItem.professors.forEach(function(profName) {
 
@@ -76,29 +76,25 @@ module.exports = function(req, res,next) {
                                 // error finding a rating
                                 console.log(profName);
                                 var course_professor_rating = {
-                                    course_num: courseItem.course_num,
-                                    course_name: courseItem.course_name,
                                     professor: profName,
                                     average_difficulty: null,
                                     average_overall: null,
                                     average_workload: null
                                 }
                                 // add card to response
-                                sendcprof(course_professor_rating);
+                                sendcprof(course_professor_rating,false);
 
 
                             } else {
                             	console.log(profName);
                                 var course_professor_rating = {
-                                    course_num: courseItem.course_num,
-                                    course_name: courseItem.course_name,
                                     professor: profName,
                                     average_difficulty: (rating.total_difficulty / rating.rating_count).toFixed(2),
                                     average_overall: (rating.total_overall / rating.rating_count).toFixed(2),
                                     average_workload: (rating.total_workload / rating.rating_count).toFixed(2)
                                 }
                                 // add card to response
-                                sendcprof(course_professor_rating);
+                                sendcprof(course_professor_rating,true);
                             }
 
                         });
@@ -108,19 +104,53 @@ module.exports = function(req, res,next) {
 
             });
 
+            function sendcprof(send,add) {
+                if(add)
+                {
+                    total_overall+=send.average_overall;
+                    total_workload+=send.average_workload;
+                    total_difficulty+=send.average_difficulty;
+                }
+                else
+                {
+                    numRating--;
+                }
+            this_sections.push(send);
+            numProf--;
+            if(numProf==0)
+            {
+               console.log(numRating);
+               console.log(total_overall);
+               console.log((total_overall/numRating).toFixed(2));
+               sendJson(courseItem.course_num, courseItem.course_name,(total_overall/numRating).toFixed(2), (total_workload/numRating).toFixed(2), (total_difficulty/numRating).toFixed(2), this_sections);
+            }
+                        
+        }
+
         });
 
         // Pass data to sendJson
-        function sendcprof(send) {
-            this_resp.push(send);
+        function sendJson(course_num, course_name, overall,workload,difficulty,sections) {
+            console.log(overall);
+            var course={
+                            course_num: course_num,
+                            course_name: course_name,
+                            course_avg_overall: overall,
+                            course_avg_difficulty: difficulty,
+                            course_avg_workload: workload,
+                            sections: sections
+                        }
+            this_resp.push(course);
             count--;
             if(count==0)
-               sendJson(this_resp);
+            {
+                return res.end(JSON.stringify(this_resp));
+            }
         }
 
     });
-    function sendJson(send){
+    /*function sendJson(send){
     	console.log(send);
     	return res.end(JSON.stringify(send));
-	}
+	}*/
 };
